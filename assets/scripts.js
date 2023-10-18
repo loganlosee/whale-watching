090a5d0bff12695a9792822b3d6ee7db5cd946c8
Logan Losee <logan.losee99@gmail.com>
2023-10-17 19:52:51 -0600

    block

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,13 @@
-const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/b7e19a37438a4999a009f0322f5442ba"));
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the latest block number
+    web3.eth.getBlockNumber()
+        .then(function(blockNumber) {
+            // Display the latest block numberon web page
+            document.getElementById("latest-block").textContent = `Latest Ethereum Block: ${blockNumber}`;
+        })
+        .catch(function(error) {
+            console.error("Error fetching the latest block:", error);
+        });
+});
